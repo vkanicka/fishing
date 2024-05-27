b80e50c019862c2e7ca145e0a893cc9a20db41fd
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-05-27 02:04:47 -0500

    All caps pixel font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Alata as MY_FONT } from 'next/font/google'
+import { Silkscreen as MY_FONT } from 'next/font/google'
 const myFont = MY_FONT(
   {
     subsets: ['latin'],
