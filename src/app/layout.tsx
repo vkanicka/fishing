c609d314ee7e0130d2e112046e8048164573bb96
VICTORIA_KANICKA <vkanicka@gmail.com>
2024-05-27 02:20:38 -0500

    Another font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Sixtyfour as MY_FONT } from 'next/font/google'
+import { Workbench as MY_FONT } from 'next/font/google'
 const myFont = MY_FONT(
   {
     subsets: ['latin'],
